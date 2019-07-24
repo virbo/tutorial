55506439b6789f91962dce0594e10973c1e31183
Yusuf Ayuba <yusufayuba@live.com>
2019-07-24 22:07:11 +0800

    #18, add js

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -23,6 +23,9 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+        'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js',
+        'js/bootstrap-notify.min.js',
+        'js/notif.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
