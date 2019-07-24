1f9fe86cdfd75bfa00b3433531b0ba8ce062cc6e
Yusuf Ayuba <yusufayuba@live.com>
2019-07-24 23:04:39 +0800

    #29, add notif.js

---
+++ views/site/notif.js
@@ -0,0 +1,25 @@
+var socket = io.connect('http://localhost:3000');
+socket.on('notif', function (data) {
+	$( "#notifikasi" ).prepend( "<p><strong>" + data.name + "</strong>: " + data.message + "</p>" );
+	//$.notify("<strong>" + data.name + "</strong>: " + data.message);
+	$.notify({
+		message: "<strong>" + data.name + "</strong>: " + data.message,
+		
+	}, {
+		type: 'success',
+		placement: {
+			from: "bottom",
+			align: "left"
+		},
+	})
+});
+
+$('#chat-form').submit(function() {
+	var nama = $("#nick_name").val();
+	var msg = $("#message-field").val();
+	if ((nama != '') && (msg != '')){
+		socket.emit('notif',{ name: nama, message: msg});
+		$("#message-field").val("");
+	}
+	return false;
+});
