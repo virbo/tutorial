bb62a547ce96c4f36c3a5734ccd3745966e7e06e
Yusuf Ayuba <yusufayuba@live.com>
2019-07-25 15:02:13 +0800

    #35, update

--- views/site/chat.php
+++ views/site/chat.php
@@ -1,7 +1,6 @@
 <?php
 
 use yii\helpers\Html;
-use yii\helpers\Url;
 
 $this->title = 'Yii2::NodeJS';
 
@@ -12,30 +11,29 @@ $this->registerJs($this->render('notif.js'), \yii\web\View::POS_READY);
         <div class="well col-lg-8 col-lg-offset-2">
             <div class="row">
                 <?= Html::beginForm(['/'],'POST', ['id' => 'chat-form']) ?>
-
                     <div class="row">
                         <div class="col-xs-3">
                             <div class="form-group">
-                                <?php echo Html::textInput('nick_name', null, [
-                                            'class' => 'form-control',
-                                            'id' => 'nick_name',
-                                            'placeholder' => 'Nick Name'
-                                        ]); ?>
+                                <?= Html::textInput('nick_name', null, [
+                                        'class' => 'form-control',
+                                        'id' => 'nick_name',
+                                        'placeholder' => 'Nick Name'
+                                ]); ?>
                             </div>
                         </div>
                         <div class="col-xs-7">
                             <div class="form-group">
-                                <?php echo Html::textInput('message', null, [
-                                            'id' => 'message-field',
-                                            'class' => 'form-control',
-                                            'placeholder' => 'Message'
-                                        ]) ?>
+                                <?= Html::textInput('message', null, [
+                                        'id' => 'message-field',
+                                        'class' => 'form-control',
+                                        'placeholder' => 'Message'
+                                ]) ?>
                             </div>
                         </div>
                         <div class="col-xs-2">
                             <div class="form-group">
-                                <?php echo Html::submitButton('Send', [
-                                'class' => 'btn btn-block btn-success'
+                                <?= Html::submitButton('Send', [
+                                        'class' => 'btn btn-block btn-success'
                                 ]) ?>
                             </div>
                         </div>
