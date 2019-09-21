19fe402e2785206bf107c39891e05128f82c1504
Yusuf Ayuba <yusufayuba@live.com>
2019-09-21 22:05:22 +0800

    #36, update findIdentityByAccessToken

--- models/User.php
+++ models/User.php
@@ -71,7 +71,8 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+        //throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+        return static::findOne(['auth_key' => $token, 'status' => self::STATUS_ACTIVE]);
     }
 
     /**
