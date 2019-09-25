789c88311ad6d86c2c82df120263baad209f1ee1
Yusuf Ayuba <yusufayuba@live.com>
2019-09-25 13:19:53 +0800

    #65, disable autologin

--- config/web.php
+++ config/web.php
@@ -28,7 +28,7 @@ $config = [
         ],
         'user' => [
             'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
+            //'enableAutoLogin' => true,
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
